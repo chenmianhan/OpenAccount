703494673a3514543e1069c63db058c23c417059
chenmianhan <mianhan1998@139.com>
2019-06-16 17:13:47 +0800

    Create openaccount.sql
    数据库创建

---
+++ openaccount.sql
@@ -0,0 +1,241 @@
+CREATE TABLE `account-info` (
+`account_info_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`ID_type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`ID_number` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`ID_issuance_date` datetime NULL DEFAULT NULL,
+`ID_overdue_date` datetime NULL DEFAULT NULL,
+`ID_licensing_authority` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`contact_address` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`postal_address` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`trans_password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`Fund_password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`business_department` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`deposit_bank` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`deposit_account` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`deposit_password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`status` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`profession` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`education` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`ID_picture` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`ID_card_inverse_side` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`risk_assessment_mark` int(3) NULL DEFAULT NULL,
+PRIMARY KEY (`account_info_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `address` (
+`aid` int(10) NOT NULL,
+`province` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`city` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`street` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`aid`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `admin_manager` (
+`super_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`admin_id` varchar(0) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`super_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `auditor_manager` (
+`auditor_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`admin_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+PRIMARY KEY (`auditor_id`, `admin_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `currency` (
+`Fund_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`currency_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`balance` decimal(65,0) NOT NULL,
+PRIMARY KEY (`Fund_id`, `currency_type`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `customer-account` (
+`customer_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`account_info_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`open_date` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+`insurance_date` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`customer_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `demo` (
+`id` int(8) NOT NULL,
+`name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`age` int(3) NULL DEFAULT NULL
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `employee` (
+`employee_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`employee_account` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`employee_password` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`Employee_type` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`employee_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `evaluation-grade` (
+`grade` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`mini_mark` int(11) NULL DEFAULT NULL,
+`max_mark` int(11) NULL DEFAULT NULL,
+PRIMARY KEY (`grade`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `fund-account` (
+`customer_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`fund_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`bank_account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`bank` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+PRIMARY KEY (`customer_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `risk-evaluation` (
+`RE_id` int(11) NOT NULL AUTO_INCREMENT,
+`content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`option_1` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`mark_1` int(11) NULL DEFAULT NULL,
+`mark_2` int(11) NULL DEFAULT NULL,
+`mark_3` int(11) NULL DEFAULT NULL,
+`mark_4` int(11) NULL DEFAULT NULL,
+`mark_5` int(11) NULL DEFAULT NULL,
+`option_2` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`option_3` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`option_4` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`option_5` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`RE_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `trade-account` (
+`customer_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`trade_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`network-dot` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+PRIMARY KEY (`customer_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
+CREATE TABLE `user` (
+`user_id` int(11) NOT NULL AUTO_INCREMENT,
+`phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`username` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`password` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`status` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+`update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`user_id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
+
