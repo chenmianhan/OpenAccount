48cb23b6e363d99292a61714fec207ab6674c14b
chenmianhan <mianhan1998@139.com>
2019-06-24 10:35:30 +0800

    “update”

---
+++ open_account/src/main/java/com/shixun/open_account/dao/AuditorDAO.java
@@ -0,0 +1,15 @@
+package com.shixun.open_account.dao;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.annotations.Param;
+
+/****
+ *@author:cmh
+ *@date: 2019/6/241027
+ *@descrption:
+ */
+public interface AuditorDAO {
+    String getSecutityIdbyAuditorId(@Param("auditor_id") String auditor_id);
+    JSONObject getSecurity(@Param("security_id")String security_id);
+    
+}
