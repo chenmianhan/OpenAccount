d3f743ebfa8569806913bb21da07c28c437f2dab
wittt <1179102664@qq.com>
2019-07-05 16:20:41 +0800

    从session中获取userid

--- open_account/src/main/java/com/practice/open_account/controller/TimeLineController.java
+++ open_account/src/main/java/com/practice/open_account/controller/TimeLineController.java
@@ -2,6 +2,7 @@ package com.practice.open_account.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.practice.open_account.service.TimeLineService;
+import com.practice.open_account.util.SessionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.alibaba.fastjson.JSONArray;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /****
  *@author:zwz
@@ -21,16 +23,18 @@ public class TimeLineController {
     @Autowired
     private TimeLineService timeLineService;
 
-    @RequestMapping(value="/timeline/get_timeline",method = POST,produces = "application/json;charset=UTF-8")
-    public JSONArray getTimeLine(@RequestBody JSONObject js)
+    @RequestMapping(value="/timeline/get_timeline",method = GET,produces = "application/json;charset=UTF-8")
+    public JSONArray getTimeLine()
     {
-        int usrId=js.getInteger("user_id");
+        String user_id= SessionUtil.getSessionAttribute().getString("user_id");
+        int usrId= Integer.parseInt(user_id);
         return timeLineService.getTimeLine(usrId);
     }
     @RequestMapping(value="/timeline/get_optional_timeline",method = POST,produces = "application/json;charset=UTF-8")
     public JSONArray getOptionalTimeLine(@RequestBody JSONObject js)
     {
-        int usrId=js.getInteger("user_id");
+        String user_id= SessionUtil.getSessionAttribute().getString("user_id");
+        int usrId= Integer.parseInt(user_id);
         String startTime=js.getString("starttime");
         String endTime=js.getString("endtime");
         return timeLineService.getOptionalTimeLine(usrId,startTime,endTime);
